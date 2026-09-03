4d641d6f5d3983ee68a1a5afaedcb9a2cc5d19e8
zrz616 <zrz616@users.noreply.github.com>
2026-09-03 09:47:54 +0000

    fix: move all options to FZF_DEFAULT_OPTS
    All common options now in FZF_DEFAULT_OPTS so they apply to all fzf invocations

--- config/fzf.toml-night.sh
+++ config/fzf.toml-night.sh
@@ -5,47 +5,30 @@
 # 颜色配置
 FZF_COLORS='fg:#cbccc6,bg:#1f2430,hl:#ffcc66,fg+:#707a8c,bg+:#191e2a,hl+:#ffcc66,info:#73d0ff,prompt:#707a8c,pointer:#cbccc6,marker:#e0e1dc,spinner:#73d0ff,header:#d4bfff,border:#4a5064,gutter:#191e2a'
 
-# 默认选项
-export FZF_DEFAULT_OPTS="--color=${FZF_COLORS} --height=60% --layout=reverse --border=rounded"
-
-# Ctrl+T - 文件搜索
-export FZF_CTRL_T_COMMAND='fdfind --type f --hidden --follow --exclude .git'
-export FZF_CTRL_T_OPTS="
---ansi
+# 默认选项 - 所有 fzf 调用都会使用
+export FZF_DEFAULT_OPTS="
+--color=${FZF_COLORS}
 --height=60%
 --layout=reverse
 --border=rounded
 --preview-window=right:60%:wrap
 --multi
---prompt='Files> '
---marker='✓'
---pointer='▶'
---color=${FZF_COLORS}
---preview='bat --color=always --style=numbers --line-range :80 {}'
---bind='tab:toggle+down'
---bind='btab:toggle+up'
---bind='ctrl-a:select-all'
---bind='ctrl-d:deselect-all'
+--prompt=~/ 
+--pointer=▶
+--marker=✓
+--bind=tab:toggle+down
+--bind=btab:toggle+up
+--bind=ctrl-a:select-all
+--bind=ctrl-d:deselect-all
 "
 
+# Ctrl+T - 文件搜索
+export FZF_CTRL_T_COMMAND='fdfind --type f --hidden --follow --exclude .git'
+export FZF_CTRL_T_OPTS="--preview='bat --color=always --style=numbers --line-range :80 {}'"
+
 # Ctrl+R - 历史搜索
-export FZF_CTRL_R_OPTS="
---ansi
---height=50%
---layout=reverse
---border=rounded
---color=${FZF_COLORS}
-"
+export FZF_CTRL_R_OPTS="--preview='echo {}' --preview-window=up:3:wrap"
 
 # Alt+C - 目录跳转
 export FZF_ALT_C_COMMAND='fdfind --type d --hidden --follow --exclude .git'
-export FZF_ALT_C_OPTS="
---ansi
---height=60%
---layout=reverse
---border=rounded
---preview-window=right:40%:wrap
---prompt='Dirs> '
---color=${FZF_COLORS}
---preview='eza --tree --level=2 --color=always {}'
-"
+export FZF_ALT_C_OPTS="--preview='eza --tree --level=2 --color=always {}'"
